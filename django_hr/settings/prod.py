e0c452c134652161669e3dadc53e0e463d65ab65
chenqiupo <chenqiupo@inspur.com>
2020-10-24 21:33:08 +0800

    修改数据库配置

--- django_hr/settings/prod.py
+++ django_hr/settings/prod.py
@@ -5,4 +5,15 @@ ALLOWED_HOSTS = ["150.109.247.43", ]
 
 DB_USER = 'root'
 DB_PORT = '3306'
-DB_PASSWORD = r'Root)f3=!.732'
+DB_PASSWORD = 'Root)f3=!.732'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': DB_NAME,
+        'HOST': 'localhost',
+        'PORT': DB_PORT,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD
+    }
+}
